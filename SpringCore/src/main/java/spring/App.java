357e3319721c60d02427dc2f52dbaaea45700b25
gopalnagarjwr@gmail.com <gopalnagarjwr@gmail.com>
2024-01-19 18:49:37 +0530

    this is dependency injection by xml configuration

---
+++ SpringCore/src/main/java/spring/App.java
@@ -0,0 +1,16 @@
+package spring;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+         ApplicationContext context =new ClassPathXmlApplicationContext("Config.xml");
+         
+         Student s1=(Student) context.getBean("st1");
+         System.out.print(s1);
+         
+    }
+}
